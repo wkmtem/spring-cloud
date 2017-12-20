a3d63acd9d70bb5ea38c6b16cf8c25d45a1c11a7
wkm <813272417@qq.com>
2017-12-21 00:36:45 +0800

    cloud微服务demo

--- src/main/java/com/spring/cloud/controller/TestController.java
+++ src/main/java/com/spring/cloud/controller/TestController.java
@@ -1,13 +1,26 @@
 package com.spring.cloud.controller;
 
+import org.apache.tomcat.util.http.RequestUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.StringTokenizer;
 
 @RestController
 public class TestController {
 
     @GetMapping("xxx")
     public String xxx() {
-        return "你好";
+        String agent = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("user-agent");
+        System.out.println(agent);
+        StringTokenizer st = new StringTokenizer(agent,";");
+        st.nextToken();
+        String userbrowser = st.nextToken();
+        System.out.println(userbrowser);
+        String useros = st.nextToken();
+        System.out.println(useros);
+        return agent;
     }
 }
