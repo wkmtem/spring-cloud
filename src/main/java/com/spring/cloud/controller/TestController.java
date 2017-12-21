83cf525f36664400af4a48ed1fcb651d5af8f4e7
wkm <813272417@qq.com>
2017-12-22 03:35:11 +0800

    cloud微服务demo

--- src/main/java/com/spring/cloud/controller/TestController.java
+++ src/main/java/com/spring/cloud/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.spring.cloud.controller;
 
-import org.apache.tomcat.util.http.RequestUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -8,19 +9,24 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.StringTokenizer;
 
+@RefreshScope
 @RestController
 public class TestController {
 
+    @Value("${sb-test.ribbon.NFLoadBalancerRuleClassName}")
+    private String rule;
+
     @GetMapping("xxx")
     public String xxx() {
-        String agent = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("user-agent");
-        System.out.println(agent);
-        StringTokenizer st = new StringTokenizer(agent,";");
-        st.nextToken();
-        String userbrowser = st.nextToken();
-        System.out.println(userbrowser);
-        String useros = st.nextToken();
-        System.out.println(useros);
-        return agent;
+//        String agent = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("user-agent");
+//        System.out.println(agent);
+//        StringTokenizer st = new StringTokenizer(agent,";");
+//        st.nextToken();
+//        String userbrowser = st.nextToken();
+//        System.out.println(userbrowser);
+//        String useros = st.nextToken();
+//        System.out.println(useros);
+//        return agent;
+        return this.rule;
     }
 }
